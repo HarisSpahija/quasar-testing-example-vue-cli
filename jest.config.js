08a2e837be3ba7533714de9b270b377da85f18a5
HarisSpahija <haris.spahija@nederlandseloterij.nl>
2022-07-22 11:51:51 +0200

    Adjusted config and installed quasar plugin

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
+const esModules = ["quasar", "quasar/lang", "lodash-es"].join("|");
+
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   testMatch: ["<rootDir>/**/*.spec.(js|jsx|ts|tsx)"],
+  transform: {
+    // See https://jestjs.io/docs/en/configuration.html#transformignorepatterns-array-string
+    [`^(${esModules}).+\\.js$`]: "babel-jest",
+  },
+  transformIgnorePatterns: [`node_modules/(?!(${esModules}))`],
 };

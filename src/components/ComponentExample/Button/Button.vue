0b677ac01505aea3d37a41d0e7cc0d01ffca8af3
HarisSpahija <haris.spahija@nederlandseloterij.nl>
2022-07-25 10:10:46 +0200

    Removed tslint disable

--- src/components/ComponentExample/Button/Button.vue
+++ src/components/ComponentExample/Button/Button.vue
@@ -6,7 +6,6 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  // eslint-disable-next-line vue/multi-word-component-names
   name: "Button",
   emits: ["click"],
   setup(props, { emit }) {
